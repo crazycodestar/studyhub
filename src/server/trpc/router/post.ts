0056c850661ac03a37a73af899e489898a40c9b3
olalekan <olamilekanadekanmbi@gmail.com>
2022-10-24 18:26:00 +0100

    create post CRUD

--- src/server/trpc/router/post.ts
+++ src/server/trpc/router/post.ts
@@ -1,4 +1,4 @@
-import { router, publicProcedure } from "../trpc";
+import { router, publicProcedure, protectedProcedure } from "../trpc";
 import { z } from "zod";
 
 export const postRouter = router({
@@ -24,6 +24,67 @@ export const postRouter = router({
           libUser: {
             some: {
               userId: input.id,
+            },
+          },
+        },
+      });
+    }),
+
+  createPost: protectedProcedure
+    .input(
+      z.object({ description: z.string().nullish(), attachment: z.string() })
+    )
+    .query(({ ctx, input }) => {
+      return ctx.prisma.post.create({
+        data: {
+          userId: ctx.session.user.id,
+          description: input.description,
+          attachment: input.attachment,
+        },
+      });
+    }),
+
+  deletePost: protectedProcedure
+    .input(z.object({ postId: z.string() }))
+    .query(({ input, ctx }) => {
+      return ctx.prisma.post.delete({
+        where: {
+          id: input.postId,
+        },
+      });
+    }),
+
+  addToLib: protectedProcedure
+    .input(z.object({ postId: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          library: {
+            create: {
+              postId: input.postId,
+            },
+          },
+        },
+      });
+    }),
+
+  removeFromLib: protectedProcedure
+    .input(z.object({ postId: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          library: {
+            delete: {
+              postId_userId: {
+                postId: input.postId,
+                userId: ctx.session.user.id,
+              },
             },
           },
         },
